add887a6d016cc1d999b7689b24ad121c4701d17
Seong-uk-An <asu970602@naver.com>
2021-09-17 22:43:57 +0900

    landing-page

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ const fourMenus = document.querySelectorAll(".menu");
 const menu = document.querySelector(".hamburger");
 const menuBubble = document.querySelector(".talk-bubble");
 
-console.log(fourMenus);
-
 function menuToggle() {
   menuBubble.classList.toggle("hidden");
 }
 
 function showMenu() {
   if (document.documentElement.clientWidth < 1000) {
-    console.log("a");
     menu.classList.remove("hidden");
     fourMenus.forEach((element) => {
       element.classList.add("hidden");
